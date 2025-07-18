f305c7264b48097708b1c1069adba9bd4718a5e4
Frank Su <frank.su@instacart.com>
2025-07-18 09:07:56 -0400

    handle font loading

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,14 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
