de97021a47820fcff744d064f7022f004685ca4b
Frank Su <frank.su@instacart.com>
2025-08-07 23:49:26 -0400

    remove unused components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,10 @@
-import { Suspense } from 'react';
 import { DashboardContent } from '@/app/dashboard/dashboard-content';
-import { LoadingOnEnter } from '@/app/components/loading-on-enter';
 import { GET_ALL_LINKED_USER_ACCOUNTS } from '@/app/queries';
 import { getClient } from '@/lib/apollo-client';
 
 export default async function Dashboard() {
   const client = getClient();
-  const result = await client.query({ query: GET_ALL_LINKED_USER_ACCOUNTS });
+  const [userAccountsResult] = await Promise.all([client.query({ query: GET_ALL_LINKED_USER_ACCOUNTS })]);
 
-  return (
-    <Suspense fallback={<LoadingOnEnter />}>
-      <DashboardContent data={result.data} />
-    </Suspense>
-  );
+  return <DashboardContent data={userAccountsResult.data} />;
 }
